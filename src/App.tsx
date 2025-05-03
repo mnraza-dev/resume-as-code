68fd5cfabe4bdfe16dd5dd8ff04e1485d4109663
Md Noorullah Raza <mnraza94@gmail.com>
2025-05-04 01:11:03 +0530

    Implement ThemeProvider for theme management and update Header for theme toggle functionality

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,18 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './pages/Home';
-
+import { ThemeProvider } from "@/context/theme-provider"
 const App: React.FC = () => {
   return (
-    <Layout>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        {/* Future routes like pricing, about, etc., go here */}
-      </Routes>
-    </Layout>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          {/* Future routes like pricing, about, etc., go here */}
+        </Routes>
+      </Layout>
+    </ThemeProvider>
   );
 };
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,34 +1,43 @@
 import React from 'react'
-import {Link}  from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import { NavigationMenu, NavigationMenuItem, NavigationMenuList } from "@/components/ui/navigation-menu";
-import { MoonIcon } from 'lucide-react';
+import { MoonIcon, SunIcon } from 'lucide-react';
 
-const Header : React.FC = () => {
+import { useTheme } from '@/context/theme-provider'
+const Header: React.FC = () => {
+  const { theme, setTheme } = useTheme()
+  const isDark = theme === "dark";
   return (
-    <header className="sticky top-0 left-0 z-50 backdrop-blur supports-[backdrop-blur]:bg-white/60 supports-[backdrop-blur]: w-full border-b px-6 py-4">
-    <div className="max-w-7xl mx-auto flex justify-between items-center">
-      <Link to="/" className="text-2xl font-bold">Resume-as-Code</Link>
-      <NavigationMenu>
-        <NavigationMenuList className="flex space-x-6">
-          <NavigationMenuItem>
-            <Link to="/" className="hover:underline">Home</Link>
-          </NavigationMenuItem>
-          <NavigationMenuItem>
-            <Link to="/pricing" className="hover:underline">Pricing</Link>
-          </NavigationMenuItem>
-          <NavigationMenuItem>
-            <Link to="/about" className="hover:underline">About</Link>
-          </NavigationMenuItem>
+    <header className='sticky top-0 z-50 border-b bg-background/95  backdrop-blur py-2 supports-[backdrop-filter]:bg-background/60 '>
+      <nav className='container mx-auto flex justify-between items-center px-4 h-16'>
+        <Link to="/" className="text-2xl font-bold">Resume-as-Code</Link>
+        <NavigationMenu>
+          <NavigationMenuList className="flex space-x-6">
+            <NavigationMenuItem>
+              <Link to="/" className="hover:underline">Home</Link>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <Link to="/pricing" className="hover:underline">Pricing</Link>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <Link to="/about" className="hover:underline">About</Link>
+            </NavigationMenuItem>
 
-          <NavigationMenuItem>
-            <Link to="/" className=" text-yellow-400">
-            <MoonIcon/>
-            </Link>
-          </NavigationMenuItem>
-        </NavigationMenuList>
-      </NavigationMenu>
-    </div>
-  </header>
+            <div onClick={() => setTheme(isDark ? "light" : "dark")} className={`flex items-center cursor-pointer transition-transform duration-500 ${isDark ? "rotate-180" : "rotate-0"}`}>
+              {isDark ? (
+                <>
+                  <SunIcon className='text-yellow-500 rotate-0 transition-all' size={20} />
+                </>
+              ) : (
+                <>
+                  <MoonIcon size={20} />
+                </>
+              )}
+            </div>
+          </NavigationMenuList>
+        </NavigationMenu>
+      </nav>
+    </header>
   )
 }
 
