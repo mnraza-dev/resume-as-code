0adc825f5de1107108d6b4763d8fe8ff998813c9
Md Noorullah Raza <mnraza94@gmail.com>
2025-05-04 00:13:39 +0530

    Add Footer component to Layout for consistent page structure

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Header from './Header';
+import Footer from './Footer';
 
 interface LayoutProps {
     children: React.ReactNode;
@@ -14,12 +15,8 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
             {/* Main Content */}
             <main className="py-8">{children}</main>
 
-            {/* Footer Section */}
-            <footer className="bg-gray-800 py-6 mt-12">
-                <div className="max-w-7xl mx-auto text-center text-gray-400">
-                    <p>&copy; 2025 Resume-as-Code. All rights reserved.</p>
-                </div>
-            </footer>
+         
+           <Footer/>
         </div>
     );
 };
