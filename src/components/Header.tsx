d07fcea56486e37ff6e8ee814a1faf4983414c1a
Md Noorullah Raza <mnraza94@gmail.com>
2025-05-04 01:01:19 +0530

    Enhance Header component with sticky positioning and backdrop blur effect

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import {Link}  from 'react-router-dom'
 import { NavigationMenu, NavigationMenuItem, NavigationMenuList } from "@/components/ui/navigation-menu";
+import { MoonIcon } from 'lucide-react';
 
 const Header : React.FC = () => {
   return (
-    <header className="w-full border-b px-6 py-4">
+    <header className="sticky top-0 left-0 z-50 backdrop-blur supports-[backdrop-blur]:bg-white/60 supports-[backdrop-blur]: w-full border-b px-6 py-4">
     <div className="max-w-7xl mx-auto flex justify-between items-center">
       <Link to="/" className="text-2xl font-bold">Resume-as-Code</Link>
       <NavigationMenu>
@@ -17,6 +18,12 @@ const Header : React.FC = () => {
           </NavigationMenuItem>
           <NavigationMenuItem>
             <Link to="/about" className="hover:underline">About</Link>
+          </NavigationMenuItem>
+
+          <NavigationMenuItem>
+            <Link to="/" className=" text-yellow-400">
+            <MoonIcon/>
+            </Link>
           </NavigationMenuItem>
         </NavigationMenuList>
       </NavigationMenu>
