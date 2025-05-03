99068512dcb4a9617df46efc53d9db850c834b80
Md Noorullah Raza <mnraza94@gmail.com>
2025-05-03 23:59:05 +0530

    Add CodeEditor component with Monaco Editor integration for JSON editing

---
+++ src/components/CodeEditor.tsx
@@ -0,0 +1,38 @@
+import { useEffect, useRef } from 'react';
+import MonacoEditor from 'react-monaco-editor';
+
+interface CodeEditorProps {
+  code: string;
+  onChange: (newCode: string) => void;
+}
+
+const CodeEditor: React.FC<CodeEditorProps> = ({ code, onChange }) => {
+  const editorRef = useRef<any>(null);
+
+  useEffect(() => {
+    if (editorRef.current) {
+      editorRef.current.editor.setValue(code);
+    }
+  }, [code]);
+
+  const handleEditorChange = (newCode: string) => {
+    onChange(newCode);
+  };
+
+  return (
+    <MonacoEditor
+      width="100%"
+      height="400"
+      language="json"
+      theme="vs-dark"
+      value={code}
+      onChange={handleEditorChange}
+      options={{
+        selectOnLineNumbers: true,
+        minimap: { enabled: false },
+      }}
+    />
+  );
+};
+
+export default CodeEditor;
